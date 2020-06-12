639835665f3f1636e3c2ab87e71047d58e62c55c
gabil-gurbanov-gadir <gabilgg@code.edu.az>
2020-06-12 22:25:38 +0400

    HomePage Decoration is done. Begining Responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,64 +1,65 @@
 $(document).ready(function() {
     window.addEventListener('load', (event) => {
         document.querySelector("#preloader").classList.add("d-none");
-    });
-    // Start Nav-menu
-    $(document).on('scroll', function(e) {
-        if (window.scrollY > 0) {
-            $('#nav').css("position", "fixed");
-        } else if (window.scrollY == 0) {
-            $('#nav').css("position", "absolute");
-        }
-    });
-    $('ul.navbar-nav li.dropdown').each(function(elem) {
-        $(this).on('mouseenter', function(e) {
-            $(this).children('ul.dropdown-menu').addClass('show');
+        // Start Nav-menu
+        $(document).on('scroll', function(e) {
+            if (window.scrollY > 0) {
+                $('#nav').css("position", "fixed");
+            } else if (window.scrollY == 0) {
+                $('#nav').css("position", "absolute");
+            }
         });
-        $(this).on('mouseleave', function() {
-            $(this).children('ul.dropdown-menu').removeClass('show');
+        $('ul.navbar-nav li.dropdown').each(function(elem) {
+            $(this).on('mouseenter', function(e) {
+                $(this).children('ul.dropdown-menu').addClass('show');
+            });
+            $(this).on('mouseleave', function() {
+                $(this).children('ul.dropdown-menu').removeClass('show');
+            })
+        });
+        $('#searchBtn').click(function(e) {
+            e.preventDefault();
+            $('#searchBtn').next().toggleClass('show');
         })
-    });
-    $('#searchBtn').click(function(e) {
-        e.preventDefault();
-        $('#searchBtn').next().toggleClass('show');
-    })
-    $('#sideBarBtn').click(function(e) {
-        e.preventDefault();
-        $('#sideBarBtn').next().toggleClass('on');
-    })
-    $('.close-side').click(function(e) {
+        $('#sideBarBtn').click(function(e) {
             e.preventDefault();
             $('#sideBarBtn').next().toggleClass('on');
         })
-        // End Nav-menu
+        $('.close-side').click(function(e) {
+                e.preventDefault();
+                $('#sideBarBtn').next().toggleClass('on');
+            })
+            // End Nav-menu
 
-    // Start Main
+        // Start Main
 
-    $('.feature-carousel').owlCarousel({
-        loop: true,
-        nav: false,
-        margin: 30,
-        dots: true,
-        autoplay: true,
-        items: 1,
-        navText: [
-            "<i class='fa fa-angle-left'></i>",
-            "<i class='fa fa-angle-right'></i>"
-        ],
-        responsive: {
-            1000: {
-                stagePadding: 100,
+        $('.feature-carousel').owlCarousel({
+            loop: true,
+            nav: false,
+            margin: 30,
+            dots: true,
+            autoplay: true,
+            items: 1,
+            responsive: {
+                1000: {
+                    stagePadding: 100,
+                }
             }
-        }
+        });
+        wow = new WOW({
+            boxClass: 'wow', // default
+            animateClass: 'animated', // default
+            offset: 0, // default
+            mobile: true, // default
+            live: true // default
+        });
+        wow.init();
+
+        $('.timer').countTo();
+        $('.fun-fact').appear(function() {
+            $('.timer').countTo();
+        }, {
+            accY: -100
+        });
     });
-    $('.testimonial-carousel').owlCarousel({
-        loop: true,
-        margin: 10,
-        items: 1,
-        responsive: {
-            1000: {
-                items: 1
-            }
-        }
-    })
 })
